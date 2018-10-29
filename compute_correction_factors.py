ed86e95c42f21c6e2c3f93e44e2122301e11afb0
Seppo Pulkkinen <spulkkin@dev.elmo.fmi.fi>
2018-10-29 08:46:32 +0200

    Initial commit

---
+++ compute_correction_factors.py
@@ -0,0 +1,4 @@
+# For a given date range and grid definition, this script computes gridded 
+# maps of radar-gauge correction factors.
+
+# ...
---
+++ fit_regression_models.py
@@ -0,0 +1,4 @@
+# For a give date range, this script fetches radar and gauge data from the 
+# FMI databases and fits a regression model for the gauge-radar adjustments.
+
+# ...
