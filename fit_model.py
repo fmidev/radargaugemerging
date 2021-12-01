cd72044baaf8eafcb5029e3f67aba3399f5e1a5b
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2021-12-02 00:21:14 +0200

    Run black

--- fit_model.py
+++ fit_model.py
@@ -1,4 +1,4 @@
-# For a give date range, this script fetches radar and gauge data from the 
+# For a give date range, this script fetches radar and gauge data from the
 # FMI databases and fits a regression model for the gauge-radar adjustments.
 
 from __future__ import print_function
@@ -17,12 +17,12 @@ import radar_archive
 # Parse command-line arguments.
 argparser = argparse.ArgumentParser()
 argparser.add_argument("startdate", type=str, help="start date (YYYYMMDDHHMM)")
-argparser.add_argument("enddate",   type=str, help="end date (YYYYMMDDHHMM)")
-argparser.add_argument("profile",   type=str, help="configuration profile to use")
+argparser.add_argument("enddate", type=str, help="end date (YYYYMMDDHHMM)")
+argparser.add_argument("profile", type=str, help="configuration profile to use")
 args = argparser.parse_args()
 
 startdate = datetime.strptime(args.startdate, "%Y%m%d%H%M")
-enddate   = datetime.strptime(args.enddate,   "%Y%m%d%H%M")
+enddate = datetime.strptime(args.enddate, "%Y%m%d%H%M")
 
 # Read the configuration files.
 config = configparser.ConfigParser(interpolation=None)
@@ -31,39 +31,43 @@ config.read(os.path.join("config", args.profile, "datasources.cfg"))
 config_radar = config["radar"]
 config_gauge = config["gauge"]
 
-browser = radar_archive.Browser(config_radar["root_path"],  config_radar["path_fmt"], 
-                                config_radar["fn_pattern"], config_radar["fn_ext"], 
-                                int(config_radar["timestep"]))
+browser = radar_archive.Browser(
+    config_radar["root_path"],
+    config_radar["path_fmt"],
+    config_radar["fn_pattern"],
+    config_radar["fn_ext"],
+    int(config_radar["timestep"]),
+)
 
 # Read radar data from the archive.
-# TODO: Check the availability of gauge data first, and based on that, read 
+# TODO: Check the availability of gauge data first, and based on that, read
 # only the radar data that is needed.
 curdate = startdate
 R = {}
 while curdate <= enddate:
-  fn = browser.listfiles(curdate)
+    fn = browser.listfiles(curdate)
 
-  print("Reading input file %s... " % os.path.basename(fn[0][0]), end="")
+    print("Reading input file %s... " % os.path.basename(fn[0][0]), end="")
 
-  sys.stdout.flush()
-  # TODO: Implement your own importer for reading the radar data. This is for 
-  # testing purposes.
-  from numpy import random
-  R[fn[1][0]] = random.normal(size=(1000, 1000))
-  #
+    sys.stdout.flush()
+    # TODO: Implement your own importer for reading the radar data. This is for
+    # testing purposes.
+    from numpy import random
 
-  print("Done.")
+    R[fn[1][0]] = random.normal(size=(1000, 1000))
+    #
 
-  curdate += timedelta(minutes=int(config_radar["timestep"]))
+    print("Done.")
+
+    curdate += timedelta(minutes=int(config_radar["timestep"]))
 
 # Read gauge data from the cldb database.
-cldb_client = cldb.CLDBClient(config_gauge["cldb_username"], 
-                              config_gauge["cldb_password"])
+cldb_client = cldb.CLDBClient(
+    config_gauge["cldb_username"], config_gauge["cldb_password"]
+)
 
-cols = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", 
-        "nvl(elstat,0)"]
-col_names = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", 
-             "elstat"]
+cols = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "nvl(elstat,0)"]
+col_names = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "elstat"]
 
 print("Querying gauge locations from the database... ", end="")
 sys.stdout.flush()
@@ -74,18 +78,18 @@ gauges = conversions.station_list_to_lonlatalt(gauges)
 # convert the lon-lat coordinates into grid coordinates (pixels)
 pr = pyproj.Proj(config_radar["projection"])
 
-x1,y1 = pr(config_radar["bbox_ll_lon"], config_radar["bbox_ll_lat"])
-x2,y2 = pr(config_radar["bbox_ur_lon"], config_radar["bbox_ur_lat"])
+x1, y1 = pr(config_radar["bbox_ll_lon"], config_radar["bbox_ll_lat"])
+x2, y2 = pr(config_radar["bbox_ur_lon"], config_radar["bbox_ur_lat"])
 
 R_shape = list(R.values())[0].shape
 
 for g in gauges:
-  lon,lat = g[1],g[2]
-  x,y = pr(lon, lat)
-  x = (x - x1) / (x2 - x1) * (R_shape[1] - 1) + 0.5
-  y = (y2 - y) / (y2 - y1) * (R_shape[0] - 1) + 0.5
-  g[1] = x
-  g[2] = y
+    lon, lat = g[1], g[2]
+    x, y = pr(lon, lat)
+    x = (x - x1) / (x2 - x1) * (R_shape[1] - 1) + 0.5
+    y = (y2 - y) / (y2 - y1) * (R_shape[0] - 1) + 0.5
+    g[1] = x
+    g[2] = y
 
 print("Done.")
 
@@ -93,28 +97,38 @@ print("Reading gauge measurements from the database... ", end="")
 sys.stdout.flush()
 
 if config_gauge["gauge_type"] == "intensity":
-  gauge_obs = cldb_client.query_precip_int(args.startdate, args.enddate, 50, 
-    sort_by_timestamp=True, convert_timestamps=True)
+    gauge_obs = cldb_client.query_precip_int(
+        args.startdate,
+        args.enddate,
+        50,
+        sort_by_timestamp=True,
+        convert_timestamps=True,
+    )
 else:
-  gauge_obs = cldb_client.query_precip_accum_fmi(args.startdate, args.enddate, 
-    int(config_gauge["accum_time"]), 50, sort_by_timestamp=True, 
-    convert_timestamps=True)
+    gauge_obs = cldb_client.query_precip_accum_fmi(
+        args.startdate,
+        args.enddate,
+        int(config_gauge["accum_time"]),
+        50,
+        sort_by_timestamp=True,
+        convert_timestamps=True,
+    )
 
 print("Done.")
 
 # Arrange gauge locations into a dictionary.
 gauges_ = {}
 for r in gauges:
-  r = tuple(r)
-  gauges_[r[0]] = r[1:]
+    r = tuple(r)
+    gauges_[r[0]] = r[1:]
 gauges = gauges_
 
 # Arrange gauge observations into a dictionary.
-#gauge_obs = dict([(r[0], r[1:]) for r in gauge_obs])
+# gauge_obs = dict([(r[0], r[1:]) for r in gauge_obs])
 gauge_obs_ = defaultdict(list)
 for r in gauge_obs:
-  r = tuple(r)
-  gauge_obs_[r[0]].append(r[1:])
+    r = tuple(r)
+    gauge_obs_[r[0]].append(r[1:])
 gauge_obs = gauge_obs_
 
 print("Collecting radar-gauge pairs... ", end="")
@@ -126,25 +140,24 @@ n_samples = 0
 
 # Collect radar-gauge pairs.
 for radar_ts in sorted(R.keys()):
-  R_cur = R[radar_ts]
-  if radar_ts in gauge_obs.keys():
-    g_cur = gauge_obs[radar_ts]
-    for g in g_cur:
-      lpnn = g[0]
-      x,y = gauges[lpnn][0],gauges[lpnn][1]
-      x_ = int(np.round(x))
-      y_ = int(np.round(y))
-      if x_ >= 0.0 and y_ >= 0.0 and x_ <= R_shape[1] and y_ < R_shape[0]:
-        r_obs = R_cur[y_, x_]
-        g_obs = g[1]
-        # TODO: Make the threshold values configurable.
-        if r_obs > 0.1 and g_obs > 0.1:
-          r_sum += r_obs
-          g_sum += g_obs
-          n_samples += 1
+    R_cur = R[radar_ts]
+    if radar_ts in gauge_obs.keys():
+        g_cur = gauge_obs[radar_ts]
+        for g in g_cur:
+            lpnn = g[0]
+            x, y = gauges[lpnn][0], gauges[lpnn][1]
+            x_ = int(np.round(x))
+            y_ = int(np.round(y))
+            if x_ >= 0.0 and y_ >= 0.0 and x_ <= R_shape[1] and y_ < R_shape[0]:
+                r_obs = R_cur[y_, x_]
+                g_obs = g[1]
+                # TODO: Make the threshold values configurable.
+                if r_obs > 0.1 and g_obs > 0.1:
+                    r_sum += r_obs
+                    g_sum += g_obs
+                    n_samples += 1
 
 print("Done.")
 
-MFB = (10*np.log10(g_sum) / (10*np.log10(r_sum)))
+MFB = 10 * np.log10(g_sum) / (10 * np.log10(r_sum))
 print("MFB = %.3f , no. samples = %d" % (MFB, n_samples))
-
--- regression.py
+++ regression.py
@@ -4,82 +4,86 @@ import itertools
 import numpy as np
 import scipy.linalg as la
 
+
 class Polynomial:
-  """D-dimensional polynomial regression model."""
+    """D-dimensional polynomial regression model."""
 
-  def __init__(self, X, Y, degree):
-    """Fit a d-dimensional polynomial to the given data.
-    
-    Parameters
-    ----------
-    X : array_like
-      Coordinates of the data points. Array of shape (n,d), where n is the 
-      number of data points and d is dimension.
-    Y : array_like
-      One-dimensional array of length n containing function values at the data 
-      points.
-    degree : int
-      Degree of the polynomial.
-    """
-    P = np.array(X)
-    V = _polyvandernd(P, degree)
+    def __init__(self, X, Y, degree):
+        """Fit a d-dimensional polynomial to the given data.
 
-    self.__C = la.lstsq(V, Y)[0]
-    self.__degree = degree
-    self.__dim    = X.shape[1]
+        Parameters
+        ----------
+        X : array_like
+          Coordinates of the data points. Array of shape (n,d), where n is the
+          number of data points and d is dimension.
+        Y : array_like
+          One-dimensional array of length n containing function values at the data
+          points.
+        degree : int
+          Degree of the polynomial.
+        """
+        P = np.array(X)
+        V = _polyvandernd(P, degree)
 
-  def __call__(self, X):
-    """Evaluate the regression polynomial at the given points.
-    
-    Parameters
-    ----------
-    X : array_like
-      Array of shape (n,d) containing n d-dimensional points.
-    
-    Returns
-    -------
-    out : array_like
-      N-dimensional array containing the function values at the given points.
-    """
-    if len(X.shape) == 1:
-      X = np.reshape(X, (len(X), 1))
+        self.__C = la.lstsq(V, Y)[0]
+        self.__degree = degree
+        self.__dim = X.shape[1]
 
-    f_X = np.zeros(X.shape[0])
-    J = _ndpolydegrees(self.__dim, self.__degree)
-    for i in range(X.shape[0]):
-      f_X[i] = 0.0
-      for k in range(pow(self.__degree+1, self.__dim)):
-        p = 1.0
-        for l in range(self.__dim):
-          p *= pow(X[i, l], J[k, l])
-        ci = _ndpolycoeffidx(J[k, :], self.__dim, self.__degree)
-        f_X[i] += self.__C[ci] * p
+    def __call__(self, X):
+        """Evaluate the regression polynomial at the given points.
 
-    return f_X
+        Parameters
+        ----------
+        X : array_like
+          Array of shape (n,d) containing n d-dimensional points.
+
+        Returns
+        -------
+        out : array_like
+          N-dimensional array containing the function values at the given points.
+        """
+        if len(X.shape) == 1:
+            X = np.reshape(X, (len(X), 1))
+
+        f_X = np.zeros(X.shape[0])
+        J = _ndpolydegrees(self.__dim, self.__degree)
+        for i in range(X.shape[0]):
+            f_X[i] = 0.0
+            for k in range(pow(self.__degree + 1, self.__dim)):
+                p = 1.0
+                for l in range(self.__dim):
+                    p *= pow(X[i, l], J[k, l])
+                ci = _ndpolycoeffidx(J[k, :], self.__dim, self.__degree)
+                f_X[i] += self.__C[ci] * p
+
+        return f_X
+
 
 def _ndpolycoeffidx(i, m, d):
-  j = 0
-  for k in range(m):
-    j += pow(d+1, m-(k+1)) * i[k]
+    j = 0
+    for k in range(m):
+        j += pow(d + 1, m - (k + 1)) * i[k]
 
-  return j
+    return j
+
 
 def _ndpolydegrees(m, d):
-  D = np.array(list(itertools.product(*itertools.repeat(range(d+1), m))))
-  mask = np.sum(np.diff(D, axis=1), axis=1) != 0
-  D = np.vstack([D, np.fliplr(D[mask, :])])
+    D = np.array(list(itertools.product(*itertools.repeat(range(d + 1), m))))
+    mask = np.sum(np.diff(D, axis=1), axis=1) != 0
+    D = np.vstack([D, np.fliplr(D[mask, :])])
 
-  return D
+    return D
+
 
 def _polyvandernd(X, deg):
-  n,m = X.shape
+    n, m = X.shape
 
-  V = np.ones((n, pow(deg+1, m)))
-  J = _ndpolydegrees(m, deg)
-  for i in range(n):
-    for l in range(pow(deg+1, m)):
-      j = _ndpolycoeffidx(J[l, :], m, deg)
-      for k in range(m):
-        V[i, j] *= pow(X[i, k], J[l, k])
+    V = np.ones((n, pow(deg + 1, m)))
+    J = _ndpolydegrees(m, deg)
+    for i in range(n):
+        for l in range(pow(deg + 1, m)):
+            j = _ndpolycoeffidx(J[l, :], m, deg)
+            for k in range(m):
+                V[i, j] *= pow(X[i, k], J[l, k])
 
-  return V
+    return V
