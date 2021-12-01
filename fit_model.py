997d7e31d4e7d7d0456a56a2e681bd03b3795cdc
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2021-12-02 00:30:58 +0200

    Remove __future__ import

--- fit_model.py
+++ fit_model.py
@@ -1,8 +1,6 @@
 # For a give date range, this script fetches radar and gauge data from the
 # FMI databases and fits a regression model for the gauge-radar adjustments.
 
-from __future__ import print_function
-
 import argparse
 from collections import defaultdict
 import configparser
