393045caa7e4b00fd2592c464428134fe9ef7c66
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2021-12-03 01:12:49 +0200

    First version of Kalman filter-based MFB model

---
+++ kalman_mfb.py
@@ -0,0 +1,75 @@
+"""This module implements the Kalman filter-based radar-gauge bias correction
+method described in:
+
+S. Chumchean, A. Seed and A. Sharma, Correcting of real-time radar rainfall
+bias using a Kalman filtering approach, Journal of Hydrology 317, 123-137,
+2006.
+
+The method is applied to the logarithmic radar-gauge mean field bias
+
+    beta = 1 / n * sum_{i=1}^n log10(G/R)
+
+where the summation is done over all radar-gauge pairs in the domain."""
+
+from numpy import linalg as la
+import numpy as np
+
+
+class KalmabFilterMFB:
+    """The Kalman filter model described in Chumchean et al., Fig. 1."""
+
+    def __init__(self, rho_beta=0.72, sigma_beta=0.068, sigma_Y=0.25):
+        """Initialize the model.
+
+        Parameters
+        ----------
+        rho_beta : float
+
+        sigma_beta : float
+            Stationary variance of the process describing the mean field bias.
+        sigma_Y : float
+            Standard deviation of the rain rauge observation noise (mm).
+
+        Notes
+        -----
+        The default values for rho_beta and sigma_beta are taken from
+        Chumchean et al."""
+        self.__beta = 0.0
+        self.__P = _sigma_w_2(rho_beta, sigma_beta)
+        self.__rho_beta = rho_beta
+        self.__sigma_beta = sigma_beta
+        self.__sigma_Y = sigma_Y
+
+    def predict(self):
+        """Compute the predicted state for the next time step."""
+        beta_minus = self.__rho_beta * self.__beta
+        P_minus = (
+            self.__rho_beta ** 2 * self.__P
+            + (1.0 - self.__rho_beta ** 2) * self.__sigma_beta ** 2
+        )
+
+        return beta_minus, P_minus
+
+    def update(self, beta_minus, P_minus, Y):
+        """Update the Kalman filter by using the predicted state
+        (beta_minus, P_minus) and the most recent observed value of the mean
+        field bias, denoted by Y. If Y is set to None (i.e. no observation
+        available), skip the update step."""
+        if Y is not None:
+            sigma_M = self.__sigma_Y ** 2 - self.__sigma_beta ** 2
+            K = _kalman_gain(P_minus, sigma_M)
+            self.__beta = beta_minus + np.dot(K, Y - beta_minus)
+            self.__P = np.dot((1 - K), P_minus)
+        else:
+            self.__beta = beta_minus
+            self.__P = (1.0 - self.__rho_beta ** 2) * self.__sigma_beta ** 2
+
+
+# the Kalman gain defined by equation (9)
+def _kalman_gain(P_minus, sigma_M):
+    return np.dot(P_minus, la.inv(P_minus + sigma_M ** 2))
+
+
+# the stationary process variance defined by equation (3)
+def _sigma_w_2(rho_beta, sigma_beta):
+    return (1.0 - rho_beta ** 2) * sigma_beta ** 2
