5554630a8ce0a6650f5270e636905c8f9b362e98
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2022-08-16 01:53:17 +0300

    New version of collect_gauge_radar_pairs.py

--- collect_radar_gauge_pairs.py
+++ collect_radar_gauge_pairs.py
@@ -8,9 +8,12 @@ import configparser
 from datetime import datetime, timedelta
 import os
 import pickle
+import yaml
 import numpy as np
 import pyproj
 import requests
+
+import importers
 import radar_archive
 
 # parse command-line arguments
@@ -34,24 +37,36 @@ config_ds.read(os.path.join("config", args.profile, "datasources.cfg"))
 config_radar = config_ds["radar"]
 config_gauge = config_ds["gauge"]
 
+with open(os.path.join("config", args.profile, "radar_locations.yaml"), "r") as f:
+    config_radarlocs = yaml.safe_load(f)
+
+radar_locs = {}
+for radar in config_radarlocs.keys():
+    radar_locs[radar] = tuple([float(v) for v in config_radarlocs[radar].split(",")])
+
+radar_timestep = int(config_radar["timestep"])
+radar_accum_period = int(config_ds["radar"]["accum_period"])
+gauge_accum_period = int(config_ds["gauge"]["accum_period"])
+
 browser = radar_archive.Browser(
     config_radar["root_path"],
     config_radar["path_fmt"],
     config_radar["fn_pattern"],
     config_radar["fn_ext"],
-    int(config_radar["timestep"]),
+    radar_timestep,
 )
 
-# read radar data from the archive
+# read radar file names from the archive
 curdate = startdate
 radar_filenames = {}
+
 while curdate <= enddate:
     fn = browser.listfiles(curdate)
     if os.path.exists(fn[0][0]):
         radar_filenames[fn[1][0]] = fn[0][0]
         print("Found input file %s." % os.path.basename(fn[0][0]))
 
-    curdate += timedelta(minutes=int(config_radar["timestep"]))
+    curdate += timedelta(minutes=radar_accum_period)
 
 # read gauge observations from SmartMet
 cols = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "nvl(elstat,0)"]
@@ -117,40 +132,86 @@ for r in gauge_obs:
     gauge_obs_[r[0]].append(r[1:])
 gauge_obs = gauge_obs_
 
-print("Collecting radar-gauge pairs... ", end="", flush=True)
+print("Collecting radar-gauge pairs:")
 
 r_thr = float(config["thresholds"]["radar"])
 g_thr = float(config["thresholds"]["gauge"])
 
 radar_gauge_pairs = {}
 
-# collect radar-gauge pairs
-for radar_ts in sorted(radar_filenames.keys()):
-    # TODO: Implement your own importer for reading the radar data. This is for
-    # testing purposes.
-    radar_rain_accum_cur = np.random.normal(size=(1000, 1000)) + 5.0
-    #
-    radar_rain_accum_shape = radar_rain_accum_cur.shape
+rgpair_attribs = config["other"]["attributes"].split(",")
 
-    if radar_ts in gauge_obs.keys():
-        g_cur = gauge_obs[radar_ts]
-        for g in g_cur:
-            fmisid = g[0]
-            x, y = gauges[fmisid][0], gauges[fmisid][1]
-            x_ = int(np.floor(x * radar_rain_accum_shape[1]))
-            y_ = int(np.floor(y * radar_rain_accum_shape[0]))
-            if (
-                x_ >= 0.0
-                and y_ >= 0.0
-                and x_ < radar_rain_accum_shape[1]
-                and y_ < radar_rain_accum_shape[0]
-            ):
-                r_obs = radar_rain_accum_cur[y_, x_]
-                g_obs = g[1]
-                if r_obs >= r_thr and g_obs >= g_thr:
-                    if not radar_ts in radar_gauge_pairs.keys():
-                        radar_gauge_pairs[radar_ts] = []
-                    radar_gauge_pairs[radar_ts].append((r_obs, g_obs))
+gauge_lonlats = dict([(v[0], (v[1], v[2])) for v in gauge_lonlat])
+
+
+def _compute_nearest_distance(gauge_lonlat):
+    pass
+
+
+# collect radar-gauge pairs
+radar_ts = startdate
+while radar_ts <= enddate:
+    importer = importers.get_method(config_radar["importer"])
+
+    # Read radar measurements from the gauge accumulation period.
+    # Here we assume that the gauge values represent accumulation from the
+    # previous n minutes.
+    num_accum_timesteps = gauge_accum_period / radar_accum_period
+    if int(num_accum_timesteps) != num_accum_timesteps:
+        raise ValueError(
+            "gauge accumulation period not divisible by radar accumulation period"
+        )
+    num_accum_timesteps = int(num_accum_timesteps)
+    num_missing = 0
+    num_found = 0
+    radar_rain_accum_cur = 0.0
+
+    for t in range(num_accum_timesteps):
+        prev_radar_ts = radar_ts - t * timedelta(minutes=radar_accum_period)
+        if not prev_radar_ts in radar_filenames.keys():
+            num_missing += 1
+        else:
+            radar_rain_rate, _ = importer(radar_filenames[prev_radar_ts])
+            radar_rain_accum_cur += radar_rain_rate
+            num_found += 1
+
+    if num_missing > int(config["missing_values"]["max_missing_radar_timestamps"]):
+        print(
+            f"Not enough previous files found for {radar_ts} for computing accumulated rainfall, skipping."
+        )
+    else:
+        print(
+            f"Computed radar accumulation for {radar_ts} by using observations from {num_found} timestamps."
+        )
+        radar_rain_accum_cur /= num_found
+        radar_rain_accum_shape = radar_rain_accum_cur.shape
+
+        if radar_ts in gauge_obs.keys():
+            g_cur = gauge_obs[radar_ts]
+            for g in g_cur:
+                fmisid = g[0]
+                x, y = gauges[fmisid][0], gauges[fmisid][1]
+                x_ = int(np.floor(x * radar_rain_accum_shape[1]))
+                y_ = int(np.floor(y * radar_rain_accum_shape[0]))
+                if (
+                    x_ >= 0.0
+                    and y_ >= 0.0
+                    and x_ < radar_rain_accum_shape[1]
+                    and y_ < radar_rain_accum_shape[0]
+                ):
+                    r_obs = radar_rain_accum_cur[y_, x_]
+                    g_obs = g[1]
+
+                    if "distance" in rgpair_attribs:
+                        dist = _compute_nearest_distance(gauge_lonlats[fmisid])
+
+                    if r_obs >= r_thr and g_obs >= g_thr:
+                        if not radar_ts in radar_gauge_pairs.keys():
+                            radar_gauge_pairs[radar_ts] = []
+                        print(r_obs, g_obs)
+                        radar_gauge_pairs[radar_ts].append((r_obs, g_obs))
+
+    radar_ts += timedelta(minutes=gauge_accum_period)
 
 print("done.")
 
