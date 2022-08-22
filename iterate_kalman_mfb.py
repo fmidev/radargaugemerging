91c0caf119d0d9f7d1aa815f7b6ab9d51ee8d079
Seppo Pulkkinen <seppo.pulkkinen@fmi.fi>
2022-08-22 19:16:17 +0300

    Add first version of Kalman MFB iteration

---
+++ iterate_kalman_mfb.py
@@ -0,0 +1,74 @@
+# python iterate_kalman_mfb.py 201708121700 test1.dat kalman_mfb_test.dat test
+
+import argparse
+import configparser
+from datetime import datetime
+import os
+import pickle
+
+import numpy as np
+
+from kalman_mfb import KalmanFilterMFB
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("date", type=str, help="date (YYYYmmddHHMM)")
+argparser.add_argument("infile", type=str, help="radar-gauge pair file")
+argparser.add_argument("outfile", type=str, help="output file")
+argparser.add_argument("profile", type=str, help="configuration profile to use")
+argparser.add_argument(
+    "--prevstatefile",
+    type=str,
+    help="file containing the previous Kalman MFB state",
+    default=None,
+)
+args = argparser.parse_args()
+
+config = configparser.ConfigParser()
+config.optionxform = lambda x: x
+config.read(os.path.join("config", args.profile, "kalman_filter_mfb.cfg"))
+
+kalman_params = dict([(k, float(v)) for (k, v) in config["kalman_params"].items()])
+
+date = datetime.strptime(args.date, "%Y%m%d%H%M")
+
+radar_gauge_pairs = pickle.load(open(args.infile, "rb"))
+
+if args.prevstatefile is None:
+    kalman_mfb = KalmanFilterMFB(**kalman_params)
+    print("No previous state file given, initialized Kalman filter from scratch.")
+else:
+    print(f"Read Kalman filter state from {args.prevstatefile}.")
+    with open(args.prevstatefile, "rb") as f:
+        prev_state = pickle.load(f)
+        kalman_mfb = prev_state["kalman_mfb"]
+        pred_state = prev_state["pred_state"]
+
+    Y = 0.0
+    n = 0
+    for ts in radar_gauge_pairs.keys():
+        if ts == date:
+            for p in radar_gauge_pairs[ts]:
+                Y += np.log10(p[1] / p[0])
+                n += 1
+
+    if n > 0:
+        Y /= n
+    else:
+        Y = np.nan
+
+    print(f"Computed log-mean field bias = {Y:.3f} from observations at {str(date)}.")
+
+    kalman_mfb.update(pred_state[0], pred_state[1], Y)
+
+    print(f"Kalman state after update = ({kalman_mfb.beta:.3f}, {kalman_mfb.P:.3f}).")
+
+beta_minus_pr, p_minus_pr = kalman_mfb.predict()
+
+print(
+    f"Next predicted Kalman state: beta_minus = {beta_minus_pr:.3f}, p_minus = {p_minus_pr:.3f}"
+)
+
+with open(args.outfile, "wb") as f:
+    print(f"Wrote updated Kalman filter to {args.outfile}.")
+    out_dict = {"kalman_mfb": kalman_mfb, "pred_state": (beta_minus_pr, p_minus_pr)}
+    pickle.dump(out_dict, f)
