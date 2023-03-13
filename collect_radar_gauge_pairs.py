9bfa632691896b9d94860aa03de05b54391f1fe2
RadAdm <radadm@radarpp-dev.fmi.fi>
2023-03-13 14:09:24 +0200

    Added run script and refactored codes

--- collect_radar_gauge_pairs.py
+++ collect_radar_gauge_pairs.py
@@ -40,199 +40,222 @@ import importers
 import radar_archive
 import util
 
-# parse command-line arguments
-argparser = argparse.ArgumentParser()
-argparser.add_argument("startdate", type=str, help="start date (YYYYMMDDHHMM)")
-argparser.add_argument("enddate", type=str, help="end date (YYYYMMDDHHMM)")
-argparser.add_argument("outfile", type=str, help="output file")
-argparser.add_argument("profile", type=str, help="configuration profile to use")
-args = argparser.parse_args()
 
-startdate = datetime.strptime(args.startdate, "%Y%m%d%H%M")
-enddate = datetime.strptime(args.enddate, "%Y%m%d%H%M")
-
-# read the configuration files
-config = configparser.ConfigParser()
-config.read(os.path.join("config", args.profile, "collect_radar_gauge_pairs.cfg"))
-
-config_ds = configparser.ConfigParser(interpolation=None)
-config_ds.read(os.path.join("config", args.profile, "datasources.cfg"))
-
-config_radar = config_ds["radar"]
-config_gauge = config_ds["gauge"]
-
-with open(os.path.join("config", args.profile, "radar_locations.yaml"), "r") as f:
-    config_radarlocs = yaml.safe_load(f)
-
-radar_locs = util.read_radar_locations(config_radarlocs)
-
-radar_timestep = int(config_radar["timestep"])
-radar_accum_period = int(config_ds["radar"]["accum_period"])
-gauge_accum_period = int(config_ds["gauge"]["accum_period"])
-
-browser = radar_archive.Browser(
-    config_radar["root_path"],
-    config_radar["path_fmt"],
-    config_radar["fn_pattern"],
-    config_radar["fn_ext"],
-    radar_timestep,
-)
-
-# read radar file names from the archive
-curdate = startdate
-radar_filenames = {}
-
-while curdate <= enddate:
-    try:
-        fn = browser.listfiles(curdate)
-
-        if os.path.exists(fn[0][0]):
-            radar_filenames[fn[1][0]] = fn[0][0]
-            # print("Found input file %s." % os.path.basename(fn[0][0]))
-    except FileNotFoundError:
-        pass
-
-    curdate += timedelta(minutes=radar_accum_period)
-
-# read gauge observations from SmartMet
-cols = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "nvl(elstat,0)"]
-col_names = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "elstat"]
-
-print("Querying gauge observations from SmartMet: ", end="", flush=True)
-
-gauge_lonlat, gauge_obs = util.query_rain_gauges(
-    startdate,
-    enddate,
-    config_gauge,
-    ll_lon=float(config["bbox"]["ll_lon"]),
-    ll_lat=float(config["bbox"]["ll_lat"]),
-    ur_lon=float(config["bbox"]["ur_lon"]),
-    ur_lat=float(config["bbox"]["ur_lat"]),
-)
-
-# convert the lon-lat coordinates into grid coordinates (pixels)
-pr = pyproj.Proj(config_radar["projection"])
-
-x1, y1 = pr(config_radar["bbox_ll_lon"], config_radar["bbox_ll_lat"])
-x2, y2 = pr(config_radar["bbox_ur_lon"], config_radar["bbox_ur_lat"])
-
-gauge_xy = set()
-for g in gauge_lonlat:
-    x, y = pr(g[1], g[2])
-    x = (x - x1) / (x2 - x1)
-    y = (y2 - y) / (y2 - y1)
-    gauge_xy.add((g[0], x, y))
-
-print(f"{len(gauge_obs)} observations from {len(gauge_xy)} gauges found.")
-
-# insert gauge locations into a dictionary
-gauges_ = {}
-for r in gauge_xy:
-    gauges_[r[0]] = r[1:]
-gauges = gauges_
-
-# insert gauge observations into a dictionary
-gauge_obs_ = defaultdict(list)
-for r in gauge_obs:
-    gauge_obs_[r[0]].append(r[1:])
-gauge_obs = gauge_obs_
-
-r_thr = float(config["thresholds"]["radar"])
-g_thr = float(config["thresholds"]["gauge"])
-
-radar_gauge_pairs = defaultdict(dict)
-
-rgpair_attribs = config["other"]["attributes"].split(",")
-
-gauge_lonlats = dict([(v[0], (v[1], v[2])) for v in gauge_lonlat])
+def run(startdate, enddate, outfile, profile):
+    
+    startdate = datetime.strptime(startdate, "%Y%m%d%H%M")
+    enddate = datetime.strptime(enddate, "%Y%m%d%H%M")
+    
+    # read the configuration files
+    config = configparser.ConfigParser()
+    config.read(os.path.join("config", profile, "collect_radar_gauge_pairs.cfg"))
+    
+    config_ds = configparser.ConfigParser(interpolation=None)
+    config_ds.read(os.path.join("config", profile, "datasources.cfg"))
+    
+    config_radar = config_ds["radar"]
+    config_gauge = config_ds["gauge"]
+    
+    with open(os.path.join("config", profile, "radar_locations.yaml"), "r") as f:
+        config_radarlocs = yaml.safe_load(f)
+    
+    radar_locs = util.read_radar_locations(config_radarlocs)
+    
+    radar_timestep = int(config_radar["timestep"])
+    radar_accum_period = int(config_ds["radar"]["accum_period"])
+    gauge_accum_period = int(config_ds["gauge"]["accum_period"])
+    
+    browser = radar_archive.Browser(
+        config_radar["root_path"],
+        config_radar["path_fmt"],
+        config_radar["fn_pattern"],
+        config_radar["fn_ext"],
+        radar_timestep,
+    )
+    
+    # read radar file names from the archive
+    curdate = startdate
+    radar_filenames = {}
+    
+    while curdate <= enddate:
+        try:
+            fn = browser.listfiles(curdate)
+    
+            if os.path.exists(fn[0][0]):
+                radar_filenames[fn[1][0]] = fn[0][0]
+                # print("Found input file %s." % os.path.basename(fn[0][0]))
+        except FileNotFoundError:
+            pass
+    
+        curdate += timedelta(minutes=radar_accum_period)
+    
+    # read gauge observations from SmartMet
+    cols = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "nvl(elstat,0)"]
+    col_names = ["lpnn", "lat", "lat_sec", "lon", "lon_sec", "grlat", "grlon", "elstat"]
+    
+    print("Querying gauge observations from SmartMet: ", end="", flush=True)
+    
+    gauge_lonlat, gauge_obs = util.query_rain_gauges(
+        startdate,
+        enddate,
+        config_gauge,
+        ll_lon=float(config["bbox"]["ll_lon"]),
+        ll_lat=float(config["bbox"]["ll_lat"]),
+        ur_lon=float(config["bbox"]["ur_lon"]),
+        ur_lat=float(config["bbox"]["ur_lat"]),
+    )
+    
+    # convert the lon-lat coordinates into grid coordinates (pixels)
+    pr = pyproj.Proj(config_radar["projection"])
+    
+    x1, y1 = pr(config_radar["bbox_ll_lon"], config_radar["bbox_ll_lat"])
+    x2, y2 = pr(config_radar["bbox_ur_lon"], config_radar["bbox_ur_lat"])
+    
+    gauge_xy = set()
+    for g in gauge_lonlat:
+        x, y = pr(g[1], g[2])
+        x = (x - x1) / (x2 - x1)
+        y = (y2 - y) / (y2 - y1)
+        gauge_xy.add((g[0], x, y))
+    
+    print(f"{len(gauge_obs)} observations from {len(gauge_xy)} gauges found.")
+    
+    # insert gauge locations into a dictionary
+    gauges_ = {}
+    for r in gauge_xy:
+        gauges_[r[0]] = r[1:]
+    gauges = gauges_
+    
+    # insert gauge observations into a dictionary
+    gauge_obs_ = defaultdict(list)
+    for r in gauge_obs:
+        gauge_obs_[r[0]].append(r[1:])
+    gauge_obs = gauge_obs_
+    
+    r_thr = float(config["thresholds"]["radar"])
+    g_thr = float(config["thresholds"]["gauge"])
+    
+    radar_gauge_pairs = defaultdict(dict)
+    
+    rgpair_attribs = config["other"]["attributes"].split(",")
+    
+    gauge_lonlats = dict([(v[0], (v[1], v[2])) for v in gauge_lonlat])
+        
+    # collect radar-gauge observation pairs
+    radar_ts = startdate
+    while radar_ts <= enddate:
+        print(
+            f"Collecting radar-gauge pairs between {enddate - timedelta(minutes=gauge_accum_period)} - {enddate}:"
+        )
+    
+        importer = importers.get_method(config_radar["importer"])
+    
+        # Read radar measurements from the gauge accumulation period.
+        # Here we assume that the gauge values represent accumulation from the
+        # previous n minutes.
+        num_accum_timesteps = gauge_accum_period / radar_accum_period
+        if int(num_accum_timesteps) != num_accum_timesteps:
+            raise ValueError(
+                "gauge accumulation period not divisible by radar accumulation period"
+            )
+        num_accum_timesteps = int(num_accum_timesteps)
+        num_missing = 0
+        num_found = 0
+        radar_rain_accum_cur = 0.0
+    
+        for t in range(num_accum_timesteps):
+            prev_radar_ts = radar_ts - t * timedelta(minutes=radar_accum_period)
+            if not prev_radar_ts in radar_filenames.keys():
+                num_missing += 1
+            else:
+                radar_rain_rate, _ = importer(radar_filenames[prev_radar_ts])
+                radar_rain_accum_cur += radar_rain_rate
+                num_found += 1
+    
+        if num_missing > int(config["missing_values"]["max_missing_radar_timestamps"]):
+            print(
+                f"  Skipping {radar_ts}: not enough previous files found for computing accumulated radar rainfall."
+            )
+        else:
+            print(
+                f"  Computed radar accumulation between {prev_radar_ts} - {radar_ts} from {num_found} time stamps."
+            )
+            radar_rain_accum_cur /= num_found
+            radar_rain_accum_shape = radar_rain_accum_cur.shape
+    
+            if radar_ts in gauge_obs.keys():
+                num_radar_gauge_pairs = 0
+                g_cur = gauge_obs[radar_ts]
+                for g in g_cur:
+                    fmisid = g[0]
+                    x, y = gauges[fmisid][0], gauges[fmisid][1]
+                    x_ = int(np.floor(x * radar_rain_accum_shape[1]))
+                    y_ = int(np.floor(y * radar_rain_accum_shape[0]))
+                    if (
+                        x_ >= 0.0
+                        and y_ >= 0.0
+                        and x_ < radar_rain_accum_shape[1]
+                        and y_ < radar_rain_accum_shape[0]
+                    ):
+                        r_obs = radar_rain_accum_cur[y_, x_]
+                        g_obs = g[1]
+    
+                        if "distance" in rgpair_attribs:
+                            dist = _compute_nearest_distance(gauge_lonlats[fmisid])
+    
+                        if r_obs >= r_thr and g_obs >= g_thr:
+                            radar_gauge_pairs[radar_ts][int(fmisid)] = (r_obs, g_obs)
+                            num_radar_gauge_pairs += 1
+    
+                print(f"  Collected {num_radar_gauge_pairs} radar-gauge pairs.")
+    
+        radar_ts += timedelta(minutes=gauge_accum_period)
+    
+    mae = 0.0
+    n = 0
+    for p1 in radar_gauge_pairs.values():
+        for p2 in p1.values():
+            mae += abs(p2[0] - p2[1])
+            n += 1
+    
+    mae = mae / n if n > 0 else np.nan
+    
+    print(f"Total number of radar-gauge pairs: {n}")
+    print(f"Mean absolute radar-gauge error: {mae}")
+    
+    print(f"Wrote output to {outfile}.")
+    
+    pickle.dump(radar_gauge_pairs, open(outfile, "wb"))
 
 
 def _compute_nearest_distance(gauge_lonlat):
     pass
 
 
-# collect radar-gauge observation pairs
-radar_ts = startdate
-while radar_ts <= enddate:
-    print(
-        f"Collecting radar-gauge pairs between {enddate - timedelta(minutes=gauge_accum_period)} - {enddate}:"
-    )
 
-    importer = importers.get_method(config_radar["importer"])
+def main():
 
-    # Read radar measurements from the gauge accumulation period.
-    # Here we assume that the gauge values represent accumulation from the
-    # previous n minutes.
-    num_accum_timesteps = gauge_accum_period / radar_accum_period
-    if int(num_accum_timesteps) != num_accum_timesteps:
-        raise ValueError(
-            "gauge accumulation period not divisible by radar accumulation period"
-        )
-    num_accum_timesteps = int(num_accum_timesteps)
-    num_missing = 0
-    num_found = 0
-    radar_rain_accum_cur = 0.0
+    run(args.startdate, args.enddate, args.outfile, args.profile)
 
-    for t in range(num_accum_timesteps):
-        prev_radar_ts = radar_ts - t * timedelta(minutes=radar_accum_period)
-        if not prev_radar_ts in radar_filenames.keys():
-            num_missing += 1
-        else:
-            radar_rain_rate, _ = importer(radar_filenames[prev_radar_ts])
-            radar_rain_accum_cur += radar_rain_rate
-            num_found += 1
+    
 
-    if num_missing > int(config["missing_values"]["max_missing_radar_timestamps"]):
-        print(
-            f"  Skipping {radar_ts}: not enough previous files found for computing accumulated radar rainfall."
-        )
-    else:
-        print(
-            f"  Computed radar accumulation between {prev_radar_ts} - {radar_ts} from {num_found} time stamps."
-        )
-        radar_rain_accum_cur /= num_found
-        radar_rain_accum_shape = radar_rain_accum_cur.shape
+if __name__ == '__main__':
 
-        if radar_ts in gauge_obs.keys():
-            num_radar_gauge_pairs = 0
-            g_cur = gauge_obs[radar_ts]
-            for g in g_cur:
-                fmisid = g[0]
-                x, y = gauges[fmisid][0], gauges[fmisid][1]
-                x_ = int(np.floor(x * radar_rain_accum_shape[1]))
-                y_ = int(np.floor(y * radar_rain_accum_shape[0]))
-                if (
-                    x_ >= 0.0
-                    and y_ >= 0.0
-                    and x_ < radar_rain_accum_shape[1]
-                    and y_ < radar_rain_accum_shape[0]
-                ):
-                    r_obs = radar_rain_accum_cur[y_, x_]
-                    g_obs = g[1]
+    # parse command-line arguments
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("startdate",
+                           type=str,
+                           help="start date (YYYYMMDDHHMM)")
+    argparser.add_argument("enddate",
+                           type=str,
+                           help="end date (YYYYMMDDHHMM)")
+    argparser.add_argument("outfile",
+                           type=str,
+                           help="output file")
+    argparser.add_argument("profile",
+                           type=str,
+                           help="configuration profile to use")
+    args = argparser.parse_args()
 
-                    if "distance" in rgpair_attribs:
-                        dist = _compute_nearest_distance(gauge_lonlats[fmisid])
+    main()
 
-                    if r_obs >= r_thr and g_obs >= g_thr:
-                        radar_gauge_pairs[radar_ts][int(fmisid)] = (r_obs, g_obs)
-                        num_radar_gauge_pairs += 1
-
-            print(f"  Collected {num_radar_gauge_pairs} radar-gauge pairs.")
-
-    radar_ts += timedelta(minutes=gauge_accum_period)
-
-mae = 0.0
-n = 0
-for p1 in radar_gauge_pairs.values():
-    for p2 in p1.values():
-        mae += abs(p2[0] - p2[1])
-        n += 1
-
-mae = mae / n if n > 0 else np.nan
-
-print(f"Total number of radar-gauge pairs: {n}")
-print(f"Mean absolute radar-gauge error: {mae}")
-
-print(f"Wrote output to {args.outfile}.")
-
-pickle.dump(radar_gauge_pairs, open(args.outfile, "wb"))
--- iterate_kalman_mfb.py
+++ iterate_kalman_mfb.py
@@ -32,75 +32,96 @@ import numpy as np
 
 from kalman_mfb import KalmanFilterMFB
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument("date", type=str, help="date (YYYYmmddHHMM)")
-argparser.add_argument("infile", type=str, help="radar-gauge pair file")
-argparser.add_argument("outfile", type=str, help="output file")
-argparser.add_argument("profile", type=str, help="configuration profile to use")
-argparser.add_argument(
-    "--prevstatefile",
-    type=str,
-    help="file containing the previous Kalman MFB state",
-    default=None,
-)
-args = argparser.parse_args()
 
-config = configparser.ConfigParser()
-config.optionxform = lambda x: x
-config.read(os.path.join("config", args.profile, "kalman_filter_mfb.cfg"))
-
-kalman_params = dict([(k, float(v)) for (k, v) in config["kalman_params"].items()])
-
-date = datetime.strptime(args.date, "%Y%m%d%H%M")
-
-radar_gauge_pairs = pickle.load(open(args.infile, "rb"))
-
-if args.prevstatefile is None:
-    kalman_mfb = KalmanFilterMFB(**kalman_params)
-    print("No previous state file given, initialized Kalman filter from scratch.")
-else:
-    print(f"Read Kalman filter state from {args.prevstatefile}.")
-    with open(args.prevstatefile, "rb") as f:
-        prev_state = pickle.load(f)
-        kalman_mfb = prev_state["kalman_mfb"]
-        pred_state = prev_state["pred_state"]
-
-    # compute MFB from the previous state
-    Y = 0.0
-    n = 0
-    for ts in radar_gauge_pairs.keys():
-        if ts == date:
-            for sid in radar_gauge_pairs[ts].keys():
-                p = radar_gauge_pairs[ts][sid]
-                Y += np.log10(p[1] / p[0])
-                n += 1
-
-    if n > 0:
-        Y /= n
+def run(date, infile, outfile, profile, prevstatefile):
+    
+    config = configparser.ConfigParser()
+    config.optionxform = lambda x: x
+    config.read(os.path.join("config", profile, "kalman_filter_mfb.cfg"))
+    
+    kalman_params = dict([(k, float(v)) for (k, v) in config["kalman_params"].items()])
+    
+    date = datetime.strptime(date, "%Y%m%d%H%M")
+    
+    radar_gauge_pairs = pickle.load(open(infile, "rb"))
+    
+    if prevstatefile is None:
+        kalman_mfb = KalmanFilterMFB(**kalman_params)
+        print("No previous state file given, initialized Kalman filter from scratch.")
     else:
-        Y = None
+        print(f"Read Kalman filter state from {prevstatefile}.")
+        with open(prevstatefile, "rb") as f:
+            prev_state = pickle.load(f)
+            kalman_mfb = prev_state["kalman_mfb"]
+            pred_state = prev_state["pred_state"]
+    
+        # compute MFB from the previous state
+        Y = 0.0
+        n = 0
+        for ts in radar_gauge_pairs.keys():
+            if ts == date:
+                for sid in radar_gauge_pairs[ts].keys():
+                    p = radar_gauge_pairs[ts][sid]
+                    Y += np.log10(p[1] / p[0])
+                    n += 1
+    
+        if n > 0:
+            Y /= n
+        else:
+            Y = None
+    
+        print(f"Computed log-mean field bias = {Y:.3f} from observations at {str(date)}.")
+    
+        kalman_mfb.update(pred_state[0], pred_state[1], Y)
+    
+        print(f"Kalman state after update = ({kalman_mfb.beta:.3f}, {kalman_mfb.P:.3f}).")
+    
+    corr_factor = 10 ** (kalman_mfb.beta + 0.5 * kalman_mfb.P)
+    
+    print(f"Estimated correction factor = {corr_factor:.03f}")
+    
+    beta_minus_pr, p_minus_pr = kalman_mfb.predict()
+    
+    print(
+        f"Next predicted Kalman state: beta_minus = {beta_minus_pr:.3f}, p_minus = {p_minus_pr:.3f}"
+    )
+    
+    with open(outfile, "wb") as f:
+        print(f"Wrote updated Kalman filter and next predicted state to {outfile}.")
+        out_dict = {
+            "kalman_mfb": kalman_mfb,
+            "pred_state": (beta_minus_pr, p_minus_pr),
+            "corr_factor": corr_factor,
+        }
+        pickle.dump(out_dict, f)
 
-    print(f"Computed log-mean field bias = {Y:.3f} from observations at {str(date)}.")
+        
+def main():
 
-    kalman_mfb.update(pred_state[0], pred_state[1], Y)
+    run(args.date, args.infile, args.outfile, args.profile, args.prevstatefile)
 
-    print(f"Kalman state after update = ({kalman_mfb.beta:.3f}, {kalman_mfb.P:.3f}).")
 
-corr_factor = 10 ** (kalman_mfb.beta + 0.5 * kalman_mfb.P)
 
-print(f"Estimated correction factor = {corr_factor:.03f}")
+if __name__ == '__main__':
 
-beta_minus_pr, p_minus_pr = kalman_mfb.predict()
-
-print(
-    f"Next predicted Kalman state: beta_minus = {beta_minus_pr:.3f}, p_minus = {p_minus_pr:.3f}"
-)
-
-with open(args.outfile, "wb") as f:
-    print(f"Wrote updated Kalman filter and next predicted state to {args.outfile}.")
-    out_dict = {
-        "kalman_mfb": kalman_mfb,
-        "pred_state": (beta_minus_pr, p_minus_pr),
-        "corr_factor": corr_factor,
-    }
-    pickle.dump(out_dict, f)
+    # parse command-line arguments                                                                                                                      
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("date",
+                           type=str,
+                           help="date (YYYYMMDDHHMM)")
+    argparser.add_argument("infile",
+                           type=str,
+                           help="radar-gauge pair file")
+    argparser.add_argument("outfile",
+                           type=str,
+                           help="output file")
+    argparser.add_argument("profile",
+                           type=str,
+                           help="configuration profile to use")
+    argparser.add_argument("prevstatefile",
+                           type=str,
+                           help="file containing the previous Kalman MFB state",
+                           default=None)
+    args = argparser.parse_args()
+    
+    main()
