0da847ee50dded6e615354c9e4fa3969bf60fcd0
RadAdm <radadm@radarpp-dev.fmi.fi>
2023-06-13 10:11:32 +0300

    Added multiplying composite with calculated factor

--- multiply_composite_with_calculated_factor.py
+++ multiply_composite_with_calculated_factor.py
@@ -8,6 +8,7 @@ import logging
 import sys
 import argparse
 import datetime
+import pickle
 
 
 def read_config(config_file):
@@ -58,24 +59,28 @@ def read_hdf5(image_h5_file):
     if test is not None:
         image_array = comp.dataset
         quantity = "DBZH"
+        dataset_path = test
     else:
         # Look for RATE array
         test = comp.select_dataset("RATE")
         if test is not None:
             image_array = comp.dataset
             quantity = "RATE"
+            dataset_path = test
         else:
             # Look for SNOWPROB array
             test = comp.select_dataset("SNOWPROB")
             if test is not None:
                 image_array = comp.dataset
                 quantity = "SNOWPROB"
+                dataset_path = test
             else:
                 # Look for ACCR array
                 test = comp.select_dataset("ACRR")
                 if test is not None:
                     image_array = comp.dataset
                     quantity = "ACRR"
+                    dataset_path = test
                 else:
                     logging.error(
                         f"DBZH, RATE, SNOWPROB or ACRR array not found in the file {image_h5_file}!"
@@ -107,33 +112,42 @@ def read_hdf5(image_h5_file):
     time = pair.value
 
     timestamp = date + time
+    
+    return image_array, quantity, timestamp, gain, offset, int(nodata), int(undetect), dataset_path
 
-    return image_array, quantity, timestamp, gain, offset, int(nodata), int(undetect)
 
-
-def convert_dtype(accumulated_image, output_conf, nodata_mask, undetect_mask):
+def convert_dtype(image_array, nodata_mask, undetect_mask, nodata, undetect, gain, offset):
     """Change output data dtype (e.g. to 16 bit unsigned integer) and rescale data if needed
 
     Keyword arguments:
-    accumulated_image --
-    output_conf --
-    nodata_mask --
-    undetect_mask --
+    image_array -- image array with physical values
+    nodata_mask -- nodata mask
+    undetect_mask -- undetect mask
+    nodata -- input/output data nodata
+    undetect -- input/output data undetect
+    gain -- input/output data scale
+    offset -- input/output data offset
 
     Return:
-    scaled_image_new_dtype --
+    scaled_image_new_dtype -- 
 
     """
-    scaled_image = (accumulated_image - output_conf["offset"]) / output_conf["gain"]
-    scaled_image[nodata_mask] = output_conf["nodata"]
-    scaled_image[undetect_mask] = output_conf["undetect"]
-    scaled_image_new_dtype = scaled_image.astype(output_conf["dtype"])
 
+    if nodata == 65535:
+        dtype = "uint16"
+    elif nodata == 255:
+        dtype = "uint8"
+        
+    scaled_image = (image_array - offset) / gain    
+    scaled_image[nodata_mask] = nodata
+    scaled_image[undetect_mask] = undetect
+    scaled_image_new_dtype = scaled_image.astype(dtype)
+    
     return scaled_image_new_dtype
 
 
 def dBZtoRR(dbz, coef):                                                                           
-    """Convert dBZ to rain rate (frontal/convective rain).                                                                                
+    """Convert dBZ to rain rate using simple formula.
      
     Keyword arguments:
     dbz -- Array of dBZ values
@@ -147,13 +161,13 @@ def dBZtoRR(dbz, coef):
     zr_b = coef["zr_b"]
     
     # Convert dBZ to rain rate RR
-    rr = 10 ** (dbz / (10 * zr_b) + (-math.log10(zr_a) / zr_b))
+    rr = np.power(10**(dbz/10) / zr_a, 1/zr_b)
     
     return rr
 
 
 def RRtodBZ(rr, coef):
-    """Convert rain rate to dBZ.
+    """Convert rain rate to dBZ using simple formula.
 
     Keyword arguments:
     rr -- rain rate
@@ -168,28 +182,53 @@ def RRtodBZ(rr, coef):
     zr_b = coef["zr_b"]
 
     # Convert RR to dBZ
-    dbz = 10 * zr_b * math.log10(rr) + math.log10(zr_a)
+    dbz = 10 * np.log10(zr_a * np.power(rr, zr_b))
+    
+    return dbz       
 
-    return rr       
 
+def read_radargauge_factor(radargauge_factor_file):
+    """ Read radargauge factor from file
 
-def overwrite_dataset_hdf5(infile, outfile):
+    Keyword arguments:
+    radargauge_factor_file -- pickle file containing dictionary of factors
+
+    Return:
+    radargauge_factor -- radargauge factor
+
+    """
+    pickle_file = open(radargauge_factor_file,"rb")
+    data = pickle.load(pickle_file)
+    radargauge_factor = data["corr_factor"]
+
+    return radargauge_factor
+    
+
+def overwrite_dataset_hdf5(infile, outfile, datapath, new_dataset):
     """ Overwrite modified dataset in hdf5 file and write everything to new file.
     
     Keyword arguments:
-    infile -- input filename
-    outfile -- output filename
+    infile -- input hdf5 filename
+    outfile -- output hdf5 filename
+    datapath -- dataset path in infile
+    new_dataset -- new dataset
 
-    """
+    """    
+    # Create a copy of the hdf5 file
+    with h5py.File(infile, 'r') as f_source:
+        with h5py.File(outfile, 'w') as f_dest:
+            # Copy all items from source file to destination file
+            for name, item in f_source.items():
+                f_source.copy(name, f_dest)
 
-    f1 = h5py.File(infile, 'r+')     # open the file
-    
-    data = f1['meas/frame1/data']       # load the data
-    data[...] = X1                      # assign new values to data
-    f1.close()                          # close the file
-   
-    
+            # Copy root-level attributes
+            for name, value in f_source.attrs.items():
+                f_dest.attrs[name] = value
 
+            # Overwrite the data in the existing dataset
+            f_dest[datapath][...] = new_dataset
+
+        
 def run(timestamp, config):
 
     config_file = f"/config/{config}/multiply_composite_with_calculated_factor.json"
@@ -204,31 +243,37 @@ def run(timestamp, config):
     else:
         input_file = f"{input_conf['path']}/{input_conf['filename'].format(timestamp=timestamp, config=config)}"
 
-    image_array, quantity, file_timestamp, gain, offset, nodata, undetect = read_hdf5(input_file)
+    image_array, quantity, file_timestamp, gain, offset, nodata, undetect, dataset_path = read_hdf5(input_file)
     
+    # Get nodata and undetect masks and then give
+    # value 0 to them
     nodata_mask = image_array == nodata
     undetect_mask = image_array == undetect
+    image_array[nodata_mask] = 0
+    image_array[undetect_mask] = 0
+    
+    # Convert to physical values
+    image_array_phys = image_array * gain + offset
     
     # Convert image arrays dBZ -> rate
-    first_image_array = dbzh_to_rate.dBZtoRATE_lut(
-        np.int_(first_image_array), lut_rr, lut_sr, snowprob
-    )
-    second_image_array = dbzh_to_rate.dBZtoRATE_lut(
-        np.int_(second_image_array), lut_rr, lut_sr, snowprob
-    )
-
-
+    image_array_rate = dBZtoRR(image_array_phys, coef)
+    
     # Multiply rain values with radargauge factor
-
+    radargauge_factor_file = f"{radargauge_conf['path']}/{radargauge_conf['filename'].format(config=config)}"
+    radargauge_factor = read_radargauge_factor(radargauge_factor_file)
+    image_array_rate = image_array_rate * radargauge_factor
 
     # Convert image arrays back to dBZ
-    
-    # Change nodata and undetect to zero and np.nan before interpolation
-    first_image_array[nodata_mask_first] = np.nan
-    first_image_array[undetect_mask_first] = 0
-    
+    image_array_dbz = RRtodBZ(image_array_rate, coef)
 
+    # Convert back to 16bit (or 8bit) unsigned integer values
+    image_array_dbz = convert_dtype(image_array_dbz, nodata_mask, undetect_mask, nodata, undetect, gain, offset)
+    
+    # Write to file
+    output_file = f"{output_conf['path'].format(year=timestamp[0:4], month=timestamp[4:6], day=timestamp[6:8])}/{output_conf['filename'].format(timestamp=timestamp, config=config)}"
+    overwrite_dataset_hdf5(input_file, output_file, dataset_path, image_array_dbz)
 
+    
 def main():
     run(options.timestamp, options.config)
 
@@ -237,10 +282,14 @@ if __name__ == "__main__":
     # Parse commandline arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--timestamp", type=str, default="202201170700", help="Input timestamp"
+        "--timestamp", type=str,
+        default="202201170700",
+        help="Input timestamp"
     )
     parser.add_argument(
-        "--config", type=str, default="ravake_composite", help="Config file to use."
+        "--config", type=str,
+        default="ravake_composite",
+        help="Config file to use."
     )
 
     options = parser.parse_args()
--- run_multiply_composite_with_calculated_factor.sh
+++ run_multiply_composite_with_calculated_factor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-INPATH="/tutka/data/dev/cache/radar/fmippn/hulehenri/accrate_composites"
-LATEST_TIMESTAMP=`ls -t ${INPATH}/*5min* | head -1 | awk -F "/" '{print $10}' | awk -F "_" '{print $1}'`
+INPATH=${INPATH:-"/tutka/data/dev/cache/radar-composite/hulehenri_3067_500m/dbzh/andre"}
+OUTPATH=${OUTPATH:-"/tutka/data/dev/cache/radar/fmippn/hulehenri/corrected_composites"}
+LATEST_TIMESTAMP=`ls -t ${INPATH}/*h5 | head -1 | awk -F "/" '{print $10}' | awk -F "_" '{print $1}'`
 CONFIG=${CONFIG:-"hulehenri"}
 TIMESTAMP=${TIMESTAMP:-${LATEST_TIMESTAMP}}
+
+# Make directories if they don't exist
+mkdir -p $OUTPATH
 
 # Build from Dockerfile
 #docker build -t radargaugemerging .
@@ -15,6 +19,8 @@ docker run \
        --env "config=$CONFIG" \
        --security-opt label=disable \
        --mount type=bind,source="$(pwd)"/config,target=/config \
+       --mount type=bind,source=/tutka/data/storage/radargaugemerging,target=/tutka/data/storage/radargaugemerging \
        --mount type=bind,source="$(pwd)"/multiply_composite_with_calculated_factor.py,target=/multiply_composite_with_calculated_factor.py \
        --mount type=bind,source=$INPATH,target=$INPATH \
+       --mount type=bind,source=$OUTPATH,target=$OUTPATH \
        radargaugemerging:latest
