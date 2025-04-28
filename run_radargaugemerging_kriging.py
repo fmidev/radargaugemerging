8f7052cb67eb04214c6613f7b3c7aa16024c4c8a
RadAdm <radadm@dev.tutka.fmi.fi>
2025-04-28 14:23:47 +0300

    Minor bug fix

--- run_radargaugemerging_kriging.py
+++ run_radargaugemerging_kriging.py
@@ -24,7 +24,7 @@ def main():
 
     # Compute kriged correction factor
     correction_factor_file = f"{args.outpath}/radargauge_corrfactor_{args.config}_{timestamp}.tif"
-    compute_kriged_correction_factor.run(kriging_model_file, timestamp, correction_factor_file, args.config)
+    compute_kriged_correction_factors.run(kriging_model_file, timestamp, correction_factor_file, args.config)
     
 
 if __name__ == '__main__':
