cca573c7a435967b18e96f63a6b4b12ac87d466e
RadAdm <radadm@dev.tutka.fmi.fi>
2025-06-02 10:18:05 +0300

    Bug fix

--- fit_kriging_model.py
+++ fit_kriging_model.py
@@ -74,9 +74,9 @@ def run(rgpairfile, outfile, profile):
             val.append(np.log10(p[1] / p[0]))
 
     if len(val) < int(config["kriging"]["min_valid_points"]):
-    raise Exception(
-        f"{len(val)} radar-gauge pairs found but {config['kriging']['min_valid_points']} required"
-    )
+        raise Exception(
+            f"{len(val)} radar-gauge pairs found but {config['kriging']['min_valid_points']} required"
+        )
 
     if config["kriging"]["time_scaling_factor"] == "auto":
         # a heuristic value to relate the standard deviations of the
