b03a01bac27359438e2243f462694c55ec2a0983
RadAdm <radadm@dev.tutka.fmi.fi>
2025-12-05 15:47:56 +0200

    Fixed geotiff writing

--- compute_kriged_correction_factors.py
+++ compute_kriged_correction_factors.py
@@ -185,7 +185,8 @@ def run(model, outtime, outfile, profile, nodata_mask, snowprob, snow_threshold=
         bounds = [ll_x, ll_y, ur_x, ur_y]
         out_rasters = np.stack([zvalues, sigmasq])
         fn = outfile + ".tif"
-        exporters.export_geotiff(fn, out_rasters, config["grid"]["projection"], bounds)
+        exporters.export_geotiff(fn, zvalues, config["grid"]["projection"], bounds)
+
     elif config["output"]["type"] == "numpy":
         np.savez_compressed(outfile, corr=zvalues.filled(), corr_var=sigmasq.filled())
     else:
